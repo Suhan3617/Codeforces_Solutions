7a863ec9a6bd6d4629dcff678e5a0fb063a643fc
Suhan3617 <suhanmakhare@gmail.com>
2025-09-27 17:24:05 +0530

    solution of 2149D

---
+++ 2149D.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define int long long
+#define in(a) \
+    int a;    \
+    cin >> a;
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define ld long double
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define fr(i, x, y) for (int i = x; i < y; i++)
+#define rfr(i, x, y) for (int i = x; i >= y; i--)
+#define vi vector<int>
+#define pii pair<int, int>
+#define vpii vector<pair<int, int>>
+#define lcm(a, b) ((a) / __gcd((a), (b)) * (b))
+#define gcd(a, b) __gcd(a, b)
+#define yess cout << "YES\n";
+#define no cout << "NO\n"
+
+int getans(vi& a){
+    if(a.empty()) return 0;
+    int pos=(int)a.size()/2,val=0;
+    fr(i,0,a.size()){
+        val+=abs(a[pos]-a[i])-abs(pos-i);
+    }
+    return val;
+}
+
+void solve()
+{
+    in(n);
+    vi a,b;
+    fr(i,1,n+1){
+        char c;
+        cin>>c;
+        if(c=='a'){
+            a.push_back(i);
+        }
+        else{
+            b.push_back(i);
+        }
+    }
+    cout<<min(getans(a),getans(b))<<endl;
+}
+
+int32_t main()
+{
+    fastio;
+    in(t);
+    while(t--){
+        solve();
+    }
+}
